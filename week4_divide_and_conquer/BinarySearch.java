421e89fbd27924a9833de607a39e987225481aef
Hunter Zhou <hunteran@foxmail.com>
2020-02-05 00:00:59 -0800

    Create BinarySearch.java
    pattern and bit operation

---
+++ week4_divide_and_conquer/BinarySearch.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class BinarySearch {
+
+    static int linearSearch(int[] a, int x) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) return i;
+        }
+        return -1;
+    }
+
+    static int binarySearch(int[] a, int x) {
+    	int start = 0;
+    	int end = a.length - 1;
+    	while (start < end) {
+    		//int mid = start + (end - start) / 2;
+    		int mid = (start + end) >> 1; //bit operation
+    		if (a[mid] < x)
+    			start = mid + 1;
+    		else if (a[mid] > x)
+    			end = mid - 1;
+    		else
+    			end = mid;
+    	}
+    	if (a[start] == x)
+    		return start;
+    	return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+          b[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            //replace with the call to binarySearch when implemented
+            System.out.print(binarySearch(a, b[i]) + " ");
+        }
+    }
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
